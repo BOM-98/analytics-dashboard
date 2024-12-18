d1a308562c19f07ce1612a63260d258c2cfdf4c4
BOM-98 <bromahon@tcd.ie>
2024-12-18 14:19:32 +1100

    chore: Update next-auth.d.ts to include additional properties in User and JWT interfaces

--- types/next-auth.d.ts
+++ types/next-auth.d.ts
@@ -3,6 +3,20 @@ import NextAuth from "next-auth";
 declare module "next-auth" {
   interface User {
     email: string;
+    id: string;
+    email: string;
+    firstName: string;
+    lastName: string;
+    company: string;
+    jobTitle: string;
+  }
+
+  interface JWT {
+    email: string;
+    firstName: string;
+    lastName: string;
+    company: string;
+    jobTitle: string;
   }
   interface Session {
     user: User & {
